726430a5dac668ce280ffe1d1ae97914e6b674d9
Rahul-Patanwadia <patanwadiarahulr@gmail.com>
2023-07-13 15:34:31 +0530

    removed node_modules folder

--- src/components/Utils/tools.js
+++ src/components/Utils/tools.js
@@ -22,16 +22,39 @@ export const CityLogo = (props) =>{
     else return template;
 }
 
-export const showToastError = (msg) =>{
+export const showToastError = (msg) =>(
     toast.error(msg,{
         position: toast.POSITION.TOP_LEFT
     })
-}
+)
 
-export const showToastSuccess = (msg) =>{
+export const showToastSuccess = (msg) =>(
     toast.success(msg,{
         position: toast.POSITION.TOP_LEFT
     })
+)
+
+export const Tag = (props) =>{
+    const template = 
+    <div style={{
+        background: props.bck? props.bck : '#fff',
+        fontSize: props.size? props.size : '15px',
+        color: props.color? props.color : '#000',
+        padding : '5px 10px',
+        display : 'inline-block',
+        fontFamily : 'Righteous',
+        ...props.add
+    }}>
+        {props.children}
+    </div>
+
+    if(props.link)
+    {
+        return <Link to={props.linkto}>
+            {template}
+        </Link>
+    }
+    return template;
 }
 
 export const logoutHandler = () =>{
