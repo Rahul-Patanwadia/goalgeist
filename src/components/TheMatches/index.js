d007caae45be9c672c37162af7bbc99e889480b6
Rahul-Patanwadia <patanwadiarahulr@gmail.com>
2023-07-31 22:59:38 +0530

    Finished the matches page with filter functionality

--- src/components/TheMatches/index.js
+++ src/components/TheMatches/index.js
@@ -11,6 +11,13 @@ import { CircularProgress } from "@mui/material";
 const TheMatches = () =>{
 
     const [matches,setMatches] = useState(null);
+    const [state, dispatch] = useReducer((prevState, nextState)=>{
+        return {...prevState,...nextState}
+    },{
+        filterMatches:null,
+        playedFilter:'All',
+        resultFilter:'All'
+    })
 
     const fetchData = async() =>{
         const querySnapshot = await getDocs(collection(db, "matches"));
@@ -19,13 +26,40 @@ const TheMatches = () =>{
             result.push({id:doc.id,...doc.data()});
         });
         setMatches(result)
+        dispatch({...state, filterMatches:result})
+    }
+
+    const showPlayed = (played) =>{
+        const list = matches.filter((match)=>{
+            return match.final === played;
+        });
+
+        dispatch({
+            ...state,
+            filterMatches: played === 'All' ? matches : list,
+            playedFilter: played,
+            resultFilter: 'All' 
+        })
+    }
+
+    const showResult = (result) =>{
+        const list = matches.filter((match)=>{
+            return match.result === result;
+        });
+
+        dispatch({
+            ...state,
+            filterMatches: result === 'All' ? matches : list,
+            playedFilter: 'All',
+            resultFilter: result
+        })
     }
 
     useEffect(()=>{
         if(!matches){
             fetchData();
         }
-    },[matches])
+    },[matches,state])
 
     return(
         <div>
@@ -33,7 +67,58 @@ const TheMatches = () =>{
                 <div className="the_matches_container">
                     <div className="the_matches_wrapper">
                         <div className="left">
-                            
+                            <div className="match_filters">
+                                <div className="match_filters_box">
+                                    <div className="tag">
+                                        Show Matches
+                                    </div>
+                                    <div className="cont">
+                                        <div className={`option ${state.playedFilter === 'All'? 'active' : ''}`}
+                                            onClick={()=>showPlayed('All')}
+                                        >
+                                            All
+                                        </div>
+                                        <div className={`option ${state.playedFilter === 'yes'? 'active' : ''}`}
+                                            onClick={()=>showPlayed('yes')}
+                                        >
+                                            Played
+                                        </div>
+                                        <div className={`option ${state.playedFilter === 'no'? 'active' : ''}`}
+                                            onClick={()=>showPlayed('no')}
+                                        >
+                                            Not Played
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="match_filters_box">
+                                    <div className="tag">
+                                        Games Result
+                                    </div>
+                                    <div className="cont">
+                                        <div className={`option ${state.resultFilter === 'All'? 'active' : ''}`}
+                                            onClick={()=>showResult('All')}
+                                        >
+                                            All
+                                        </div>
+                                        <div className={`option ${state.resultFilter === 'W'? 'active' : ''}`}
+                                            onClick={()=>showResult('W')}
+                                        >
+                                            W
+                                        </div>
+                                        <div className={`option ${state.resultFilter === 'L'? 'active' : ''}`}
+                                            onClick={()=>showResult('L')}
+                                        >
+                                            L
+                                        </div>
+                                        <div className={`option ${state.resultFilter === 'D'? 'active' : ''}`}
+                                            onClick={()=>showResult('D')}
+                                        >
+                                            D
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <MatchesList matches={state.filterMatches}/>
                         </div>
                         <div className="right">
                             <LeagueTable/>
